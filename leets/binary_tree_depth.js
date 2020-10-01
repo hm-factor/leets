1d6b5d53a4f5d3156f1d80efeb1764a2e630abde
hm-factor <hsmanel@gmail.com>
2020-10-01 00:59:04 -0400

    more effecient solution

--- leets/binary_tree_depth.js
+++ leets/binary_tree_depth.js
@@ -22,3 +22,14 @@ var maxDepth = function (root) {
 
   return sumDepth(root, 0);
 };
+
+// this solution is a little cleaner
+var maxDepth = function (root) {
+  if (root === null) {
+    return 0;
+  };
+
+  let left = maxDepth(root.left);
+  let right = maxDepth(root.right);
+  return Math.max(left, right) + 1;
+};
