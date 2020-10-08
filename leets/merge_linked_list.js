0cabf245bb71d81d9e69beec58980972d9e966e7
hm-factor <hsmanel@gmail.com>
2020-10-08 12:36:31 -0400

    helpful comments

--- leets/merge_linked_list.js
+++ leets/merge_linked_list.js
@@ -11,10 +11,14 @@
  * @return {ListNode}
  */
 var mergeTwoLists = function (l1, l2) {
+  // need a placeholder linked list to attach nodes of l1 and l2 to
   let dummy = new ListNode(-1);
+  // the head is a pointer to the initiated node so that we can preserve it for later
+  // otherwise we have a constantly updating listnode which isnt helpful
   let head = dummy;
 
   while (l1 !== null && l2 !== null) {
+    // these assignments arent necessary but somehow increased runtime by 20ms
     let x = l1 ? l1.val : 0;
     let y = l2 ? l2.val : 0;
 
