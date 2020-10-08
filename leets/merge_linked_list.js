ad5679693313242626045ffb34632f59bc8d1ae0
hm-factor <hsmanel@gmail.com>
2020-10-08 12:34:39 -0400

    merge linked list solved

--- leets/merge_linked_list.js
+++ leets/merge_linked_list.js
@@ -11,21 +11,34 @@
  * @return {ListNode}
  */
 var mergeTwoLists = function (l1, l2) {
-  let output = [];
+  let dummy = new ListNode(-1);
+  let head = dummy;
 
-  while (l1 || l2) {
+  while (l1 !== null && l2 !== null) {
     let x = l1 ? l1.val : 0;
     let y = l2 ? l2.val : 0;
 
-    if (x >= y) {
-      output.push(x);
-      l1 = l1.next
+    if (x <= y) {
+      dummy.next = l1;
+      if (l1) {
+        l1 = l1.next
+      };
     } else {
-      output.push(y);
-      l2 = l2.next
+      dummy.next = l2;
+      if (l2) {
+        l2 = l2.next
+      };
     };
+
+    dummy = dummy.next;
+  };
+
+  if (l1 === null) {
+    dummy.next = l2;
+  } else if (l2 === null) {
+    dummy.next = l1;
   };
 
 
-  return output
+  return head.next
 };
