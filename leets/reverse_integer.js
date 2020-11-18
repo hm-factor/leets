c28b9d790db0cb5e76ce0635b774b4509a7cfef5
hm-factor <hsmanel@gmail.com>
2020-11-18 10:45:07 -0500

    checking for negative

--- leets/reverse_integer.js
+++ leets/reverse_integer.js
@@ -4,5 +4,16 @@
  */
 
 var reverse = function(x) {
-   
+   if(x === 0) return 0;
+
+       
+    let neg = false;
+    
+    if(x < 0) { neg = true };
+    
+    let temp = x.toString();
+    let arr = temp.split("");
+    if(arr[0] === '-') {arr.shift()}
+    
+    
 };
