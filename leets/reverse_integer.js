bfd924d0c225faf685c147d0cd59a683d5a15f59
hm-factor <hsmanel@gmail.com>
2020-11-18 10:59:06 -0500

    takes care of negative numbers - doesnt deal with range

--- leets/reverse_integer.js
+++ leets/reverse_integer.js
@@ -4,9 +4,8 @@
  */
 
 var reverse = function(x) {
-   if(x === 0) return 0;
-
-       
+    if(x === 0) return 0;
+    
     let neg = false;
     
     if(x < 0) { neg = true };
@@ -15,5 +14,17 @@ var reverse = function(x) {
     let arr = temp.split("");
     if(arr[0] === '-') {arr.shift()}
     
+    let newArr = [];
+    for(let i=arr.length; i--; i >= 0) {
+        newArr.push(arr[i]);
+    }
     
+    let reverseStr = newArr.join("")
+    
+    if(neg) {
+        let posInt = parseInt(reverseStr);
+        return -1*posInt
+    } else {
+        return parseInt(reverseStr);
+    }
 };
