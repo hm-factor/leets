c861da8fd31775fc9fdcd6ce33c206f0bc92495f
hm-factor <hsmanel@gmail.com>
2021-01-28 16:17:33 -0500

    first pass

--- leets/lists/water_area.py
+++ leets/lists/water_area.py
@@ -4,3 +4,21 @@ class Solution(object):
         :type height: List[int]
         :rtype: int
         """
+        min = 0
+        result = 0
+
+        if len(height) == 0 or len(height) == 1:
+            return 0
+
+        for idx, j in enumerate(height):
+            idx1 = idx
+            min = j
+            for idx, k in enumerate(height):
+                idx2 = idx
+                if k < j:
+                    min = k
+                diff = abs(idx2 - idx1)
+                if (diff*min) > result:
+                    result = (diff*min)
+
+        return result
