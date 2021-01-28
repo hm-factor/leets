566431c81bd004ef1b62ab0ec1e220fc110c3c6d
hm-factor <hsmanel@gmail.com>
2021-01-28 17:55:42 -0500

    linear solution

--- leets/lists/water_area.py
+++ leets/lists/water_area.py
@@ -4,15 +4,37 @@ class Solution(object):
         :type height: List[int]
         :rtype: int
         """
-        result = 0
 
-        if len(height) <= 1:
-            return 0
+        ### SLIDING POINTERS ###
 
-        for i1, j in enumerate(height):
-            for i2, k in enumerate(height):
-                small = min(j, k)
-                diff = abs(i1 - i2)
-                result = max(result, diff*small)
+        left = 0
+        right = len(height) - 1
+        area = 0
 
-        return result
+        while left < right:
+            small = min(height[left], height[right])
+            area = max(area, small*(right - left))
+
+            if height[left] > height[right]:
+                right = right - 1
+            else:
+                left = left + 1
+
+        return area
+
+        ### ENUMERATION ###
+
+        # result = 0
+
+        # if len(height) <= 1:
+        #     return 0
+
+        # for i1, j in enumerate(height):
+        #     for i2, k in enumerate(height):
+        #         small = min(j, k)
+        #         diff = abs(i1 - i2)
+        #         result = max(result, diff*small)
+
+        # return result
+
+
