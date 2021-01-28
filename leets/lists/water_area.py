d2b834d9fb3a293528b916f9d91ee16d9f7dcf1b
hm-factor <hsmanel@gmail.com>
2021-01-28 16:27:43 -0500

    second pass - still n^2

--- leets/lists/water_area.py
+++ leets/lists/water_area.py
@@ -4,21 +4,15 @@ class Solution(object):
         :type height: List[int]
         :rtype: int
         """
-        min = 0
         result = 0
 
-        if len(height) == 0 or len(height) == 1:
+        if len(height) <= 1:
             return 0
 
-        for idx, j in enumerate(height):
-            idx1 = idx
-            min = j
-            for idx, k in enumerate(height):
-                idx2 = idx
-                if k < j:
-                    min = k
-                diff = abs(idx2 - idx1)
-                if (diff*min) > result:
-                    result = (diff*min)
+        for i1, j in enumerate(height):
+            for i2, k in enumerate(height):
+                small = min(j, k)
+                diff = abs(i1 - i2)
+                result = max(result, diff*small)
 
         return result
