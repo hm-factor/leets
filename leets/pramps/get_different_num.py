c7d7a1362d6b5f683363db944e5099ad15642aae
hm-factor <hsmanel@gmail.com>
2021-01-29 21:49:42 -0500

    get diff num pramp

---
+++ leets/pramps/get_different_num.py
@@ -0,0 +1,22 @@
+def get_different_number(arr):
+
+  for el in range(0, len(arr)):
+    if el not in arr:
+      return el
+
+  return len(arr)
+
+# unique pos integers and 0
+# if arr doesnt have 0, output = 0
+
+# if length is one more than greatest num, answer is length
+
+
+# mid = [0,1000,2,1,4,5,6]
+
+# for i=0 i<len
+# if set.has(i)
+#   return i
+
+# return length arr
+
