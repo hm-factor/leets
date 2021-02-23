d51d7980a5671fd728f03a379395f42275e80306
hm-factor <hsmanel@gmail.com>
2021-02-22 23:52:04 -0500

    comments

--- leets/py_practice/lists/sequential_digits.py
+++ leets/py_practice/lists/sequential_digits.py
@@ -1,7 +1,14 @@
 class Solution:
     def sequentialDigits(self, low, high):
 
-'''
-  Input: low = 100, high = 300
-  Output: [123,234]
-'''
+      # change into str, iterate over and add 1 as you go through
+      # keeping track of the previous digit
+
+      # variable keeps track of the previous number, 'while num < high' do all logic
+
+#   Input: low = 100, high = 300
+#   Output: [123,234]
+
+#   Input: low = 1000, high = 13000
+#   Output: [1234, 2345, 3456, 4567, 5678, 6789, 12345]
+
