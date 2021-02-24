5eb619b90f71a552e8a7a1a5849f049ce7cba12a
hm-factor <hsmanel@gmail.com>
2021-02-23 23:47:58 -0500

    running into str to int conversion issues

--- leets/py_practice/lists/sequential_digits.py
+++ leets/py_practice/lists/sequential_digits.py
@@ -12,3 +12,15 @@ class Solution:
 #   Input: low = 1000, high = 13000
 #   Output: [1234, 2345, 3456, 4567, 5678, 6789, 12345]
 
+        nums = '123456789'
+        out = []
+        
+        i = len(str(low))
+        while i <= len(nums):
+            for j in range(len(nums)):
+                curr = nums[j+1:i]
+                if low <= int(curr) and int(curr) <= high:
+                    out.append(curr)
+                    low = curr
+                i += 1
+        print(out)
