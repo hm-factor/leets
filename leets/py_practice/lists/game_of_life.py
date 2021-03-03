909639bc85d0424e7a8976cdd3537a2aa2a06674
hm-factor <hsmanel@gmail.com>
2021-03-02 23:16:33 -0500

    comments

--- leets/py_practice/lists/game_of_life.py
+++ leets/py_practice/lists/game_of_life.py
@@ -2,6 +2,10 @@ class Solution:
     def gameOfLife(self, board: List[List[int]]) -> None:
         """
         Do not return anything, modify board in-place instead.
+        
+        find all surrounding alive tiles on the board (1)
+
+        change to a 2 if it was a 1, and -1 if it was 0
         """
 
     dirs = [(-1,0), (-1,1), (0,1), (1,1), (1,0), (1,-1), (0,-1), (-1,-1)]
