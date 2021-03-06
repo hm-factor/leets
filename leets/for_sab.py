45698fe47d82ba3f05366d453e03bd2c6938caf2
hm-factor <hsmanel@gmail.com>
2021-03-05 19:19:15 -0500

    binary search first pass

--- leets/for_sab.py
+++ leets/for_sab.py
@@ -6,3 +6,15 @@
   5. given a list of words, return a dict whose keys are ints and values are words that are that long
   6. given an amount of money, return every different ways that can be represented using coins
 """
+
+def binary_search(nums: list, target: int) -> bool:
+    mid = len(nums)/2
+
+    if target < nums[mid]:
+      return binary_search(nums[0:mid], target)
+    elif target > nums[mid]:
+      return binary_search(nums[mid + 1:], target):
+    elif target == nums[mid]:
+      return True
+    
+    return False
