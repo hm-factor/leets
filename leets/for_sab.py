dcf47bf2492e1116a402589263ea832a0b12ccb2
hm-factor <hsmanel@gmail.com>
2021-03-05 19:12:59 -0500

    sabrina questions

---
+++ leets/for_sab.py
@@ -0,0 +1,8 @@
+"""
+  1. recursive binary search
+  2. decimal representation of fractions (f strings)
+  3. make two words out of one with every other letter: waists -> wit + ass, board -> bad + or
+  4. sums int value of all letters in file
+  5. given a list of words, return a dict whose keys are ints and values are words that are that long
+  6. given an amount of money, return every different ways that can be represented using coins
+"""
