1bee97d0e63a05a66f8acf395e864fcbc1b1dd93
hm-factor <hsmanel@gmail.com>
2021-03-06 10:25:57 -0500

    sum lines

--- leets/for_sab.py
+++ leets/for_sab.py
@@ -1,11 +1,13 @@
 """
   1. recursive binary search
-  2. decimal representation of fractions (f strings)
+  2. decimal representation of fractions (f strings -> f'str{variables}str')
   3. make two words out of one with every other letter: waists -> wit + ass, board -> bad + or
   4. sums int value of all letters in file
   5. given a list of words, return a dict whose keys are ints and values are words that are that long
   6. given an amount of money, return every different ways that can be represented using coins
 """
+
+import string
 
 def binary_search(nums: list, target: int) -> bool:
   mid = len(nums)//2
@@ -20,6 +22,20 @@ def binary_search(nums: list, target: int) -> bool:
   
   return False
 
-arr = [1,2,3,4,5,6,7]
+# arr = [1,2,3,4,5,6,7]
 
-print(binary_search(arr, 4))
+# print(binary_search(arr, 4))
+
+def sum_lines(fname: str) -> dict:
+  alph = {chr(x+96):x for x in range(1,27)}
+  sum = 0
+  with open(fname) as f:
+    # content = f.read().splitlines()
+    for line in f:
+      line = line.strip()
+      for ch in line:
+        if ch != " ":
+          sum += alph[ch]
+  return sum
+
+# print(sum_lines('./file.txt'))
