5773675dbeaf40981528066d3b22d24ee12128a5
hm-factor <hsmanel@gmail.com>
2022-01-11 23:02:21 -0500

    better solution w cool tuple impementation

--- leets/py_practice/dynamic_programming/fib.py
+++ leets/py_practice/dynamic_programming/fib.py
@@ -15,3 +15,14 @@ def fib(self, n: int) -> int:
         t += 1
 
     return memo[n]
+
+# awesome tuple solution w memoization
+def fib(self, n: int) -> int:
+    if n == 0:
+        return 0
+
+    memo = (0, 1)
+    for _ in range(2, n+1):
+        memo = (memo[-1], memo[-1] + memo[-2])
+
+    return memo[-1]
